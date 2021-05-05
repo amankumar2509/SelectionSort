11216a1f43cc3278cf876a68ee41ea56e8ca5327
amankumar2509 <amankumar.1_cs19@gla.ac.in>
2021-05-05 23:57:21 +0530

    selectionSort() method completed

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -8,6 +8,19 @@
 
 public class SelectionSort {
     public void selectionSort(int[] array){
+        int n= array.length;
+        for (int i = 0; i <n-1 ; i++) {
+            int minimumValue=i;
+            for (int j = i+1; j <n ; j++) {
+                if(array[j]<array[minimumValue]){
+                    minimumValue=j;
+                }
+
+            }
+            int temp = array[i];
+            array[i] = array[minimumValue];
+            array[minimumValue] = temp;
+        }
 
     }
 
